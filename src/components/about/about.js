1f6ceafdae1a8538edad016cff85dba39a301af2
Stewart Mangame <stewartmangame8@gmail.com>
2024-05-21 08:38:18 +0200

    changed css stylish to tailwind and modified files

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import './about.css';
 import pic from './pic.png'
 
 function About(){
     return(
-        <div className='About'>
-            <img className='pic' src={pic} alt='pic'/>
-            <h1 className='wel'>WELCOME TO MY FARM</h1>
-            <p className='mission'>Our mission is to empower local farmers and producers by providing them with access to innovatives<br></br>
+        <div className='@apply bg-[rgb(226,214,214)]'>
+            <img className='@apply w-full h-[300px] object-cover' src={pic} alt='pic'/>
+            <h1 className='@apply text-[green] font-bold text-[x-large] m-0 pt-[0.5%] p-0'>WELCOME TO MY FARM</h1>
+            <p className='@apply font-[450] m-0 p-0'>Our mission is to empower local farmers and producers by providing them with access to innovatives<br></br>
                tools, news, markets, loans, accesing consultation, registering Cooperatives and applying for subsidy.<br></br>
                We aim to to foster a supportive community that encourage collaborations and knowledge sharing,    <br></br>
                Ultimatery enhasing the productivity and profitability of agriculture venture across Malawi. 
--- src/components/pages/home1.js
+++ src/components/pages/home1.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import Slideshow from '../slider/slider'
 
-import './home2.css'
-
 
 export default function Home1() {
   return (
     <div>
       
-      <div className='new'>
-      <h1 className='news'>LATEST NEWS</h1>
-      <p className='dic'>Stay informed about agriculture news article on global agriculture and case studies and insights on farmers around Africa,<br></br> the latest future price, livestock future, markets trends, weather forecast,
+      <div className='@apply bg-[rgb(226,214,214)]'>
+      <h1 className=' @apply text-[green] font-bold text-[large] m-0 p-0'>LATEST NEWS</h1>
+      <p className='@apply m-0 pb-[2%] p-0'>Stay informed about agriculture news article on global agriculture and case studies and insights on farmers around Africa,<br></br> the latest future price, livestock future, markets trends, weather forecast,
         availble loans and etc.<br></br> 
       </p>
       </div>
       <Slideshow/>
-      <div className='button'>
-      <button className='bt'><a href='https://times.mw/'> view more</a></button>
+      <div className='@apply bg-[rgb(226,214,214)] pt-[2%] pb-[1%]'>
+      <button className=' @apply bg-[orange] font-bold w-[15%] h-[30%] text-[medium] rounded-[40%]'><a href='https://times.mw/'> view more</a></button>
       </div>
     </div>
     
