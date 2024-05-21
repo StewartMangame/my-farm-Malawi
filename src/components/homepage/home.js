c2639fd2653d8c490d77e128b3256a9e981205af
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-21 00:39:06 -0700

    Added footer.js to our homepage folder and all pages that needs it can simlpy import and use it but make sure to place download all othe contents

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import Header from '../homepage/header'
+import React from "react";
+import Header from "../homepage/header";
+import Footer from "../homepage/footer";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Footer />
     </div>
-  )
+  );
 }
