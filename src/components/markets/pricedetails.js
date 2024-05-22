86b0ea994b3610bf58732b7bb96d6d2edf2f421e
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-21 23:22:01 -0700

    Created a productdetails.js file which displays products and thieir respective prices in markets branch

--- src/components/markets/pricedetails.js
+++ src/components/markets/pricedetails.js
@@ -1,24 +1,54 @@
-const Pricedetails = () => {
+import React from "react";
+
+const Productdetails = () => {
+  const products = [
+    { name: "Chimanga", price: 2600 },
+    { name: "Mpunga", price: 2200 },
+    { name: "Nyemba", price: 3400 },
+    { name: "Chinangwa", price: 1800 },
+    { name: "Mtedza", price: 3400 },
+    { name: "Nandolo", price: 1500 },
+    { name: "Soya", price: 2800 },
+    { name: "Mawere", price: 800 },
+  ];
+
   return (
-    <div>
-      <div>
-        <h1>
-          {" "}
-          <img
-            src="https://th.bing.com/th/id/OIP.rMyXRPUMXozV-k2r3__AcAHaHq?w=184&h=191&c=7&r=0&o=5&pid=1.7"
-            alt="malawi logo"
-            className="align-left pl-[38%] h-[120px]"
-          />
-          MALAWI GOVERNMENT LEGAL AGRI-PRODUCTS PRICES
-          <img
-            src="https://th.bing.com/th/id/R.8830b3084a547dda79983e0a2bba5a4c?rik=eKYByYIXrdwj%2fw&pid=ImgRaw&r=0"
-            alt="malwi logo 2"
-            className="aligh pl-[38%] h-[120px]"
-          />
-        </h1>
+    <div className="container mx-auto p-4">
+      <h1 className="text-[bold] text-[2.5em] text-center text-[green]">
+        {" "}
+        <img
+          src="https://th.bing.com/th/id/OIP.rMyXRPUMXozV-k2r3__AcAHaHq?w=184&h=191&c=7&r=0&o=5&pid=1.7"
+          alt="malawi logo"
+          className="align-left pl-[38%] h-[120px] pl-[45%]"
+        />
+        MALAWI GOVERNMENT LEGAL AGRI-PRODUCTS PRICES
+      </h1>
+      <table className="w-full bg-gray-300 rounded-lg shadow-md">
+        <thead>
+          <tr className="bg-green-300">
+            <th className="py-2 px-4 text-left">Product Name</th>
+            <th className="py-2 px-4 text-left">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={index}>
+              <td className="py-2 px-4">{product.name}</td>
+              <td className="py-2 px-4">K {product.price}/kg</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-4 flex justify-end">
+        <a
+          href="https://www.admarc.co.mw"
+          className="text-indigo-600 hover:underline"
+        >
+          View Market Details
+        </a>
       </div>
     </div>
   );
 };
 
-export default Pricedetails;
+export default Productdetails;
