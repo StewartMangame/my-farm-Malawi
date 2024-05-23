db5674063fb6538fd7d1fe682bc7ac1813ad0daa
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-23 02:31:57 -0700

    Added pages to routes.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/homepage/home";
 import Markets from "./components/markets/markets";
+import Footer from "./components/homepage/footer";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Markets />
+      <Footer />
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,53 +1,64 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/homepage/home.js";
-import Cooperatives from "./components/cooperatives/cooperative.js";  
-import Cooperativeform from "./components/cooperatives/cooperativeform"
+import Cooperatives from "./components/cooperatives/cooperative.js";
+import Cooperativeform from "./components/cooperatives/cooperativeform";
 import Loans from "./components/loans/loans";
 import Loanshome from "./components/loans/loanshome";
+import Admarc from "./components/markets/admarcproducts.js";
+import EtgProducts from "./components/markets/etgproducts.js";
+import Farmersworld from "./components/markets/farmersworld.js";
+import Productdetails from "./components/markets/pricedetails.js";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/cooperatives",
+    element: <Cooperatives />,
+  },
+  {
+    path: "/cooperativeform",
+    element: <Cooperativeform />,
+  },
+  {
+    path: "/admarc",
+    element: <Admarc />,
+  },
 
-    {
-        path:"/",
-        element: <Home/> 
-    },
-    {
-        path:"/cooperatives",
-        element:<Cooperatives/>
-    },
-    {
-        path:"/cooperativeform",
-        element: <Cooperativeform/>
-    },
+  // const router = createBrowserRouter([
+  //     {
+  //         path:"/",
+  //         element:<Home />
 
-
-
-
-
-// const router = createBrowserRouter([
-//     {
-//         path:"/",
-//         element:<Home />
-
-
-
-{
-    path:"/loans",
-    element:<Loans />
-},
-{
-path:"/loanshome",
-element:<Loanshome/>
-
-},
-//    {
-//         path: '/login',
-//         element: <Login/>
-//     },
-//     {
-//         path: '/register',
-//         element: <Register/>
-    ]);
-
+  {
+    path: "/loans",
+    element: <Loans />,
+  },
+  {
+    path: "/loanshome",
+    element: <Loanshome />,
+  },
+  {
+    path: "/etgproducts",
+    element: <EtgProducts />,
+  },
+  {
+    path: "/farmersworld",
+    element: <Farmersworld />,
+  },
+  {
+    path: "/pricedetails",
+    element: <Productdetails />,
+  },
+  //    {
+  //         path: '/login',
+  //         element: <Login/>
+  //     },
+  //     {
+  //         path: '/register',
+  //         element: <Register/>
+]);
 
 export default router;
