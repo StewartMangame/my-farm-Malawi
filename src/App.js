97795ccd5fe72dbe27044dc16b6f903a92c18119
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-22 23:34:32 -0700

    Resolve merge conflict by incorporating both suggestions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import Home from "./components/homepage/home";
-import Loans from "./components/loans/loans";
 import Markets from "./components/markets/markets";
-import SubsidyApplicationForm from "./components/subsidy-application/subsidyApplicationForm";
-import Consultation from "./components/consultation/consultation";
-import Cooperatives from "./components/cooperatives/cooperative";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <Loans />
-      <Consultation />
-      <Cooperatives />
-      <SubsidyApplicationForm />
       <Markets />
     </div>
   );
